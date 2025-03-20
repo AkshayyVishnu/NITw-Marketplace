aa15ac75aa6ff5d1736711a8e56654654dea0300
AkshayyVishnu <vish4Akshay@gmail.com>
2025-03-21 04:05:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,40 @@
-// Theme Toggle
 document.addEventListener("DOMContentLoaded", function () {
-    const storedTheme = localStorage.getItem("theme") || "light-theme";
-    document.body.className = storedTheme;
-  });
-  
-  document.getElementById("toggleTheme").addEventListener("click", function () {
-    if (document.body.classList.contains("light-theme")) {
-      document.body.classList.remove("light-theme");
-      document.body.classList.add("dark-theme");
-      localStorage.setItem("theme", "dark-theme");
-    } else {
-      document.body.classList.remove("dark-theme");
-      document.body.classList.add("light-theme");
-      localStorage.setItem("theme", "light-theme");
-    }
-  });
-  
-  // Typewriter Effect
-  const typedElement = document.querySelector(".typed-animation h1");
-  const text = "NITw Marketplace Welcomes you!";
-  let index = 0;
-  
-  function typeLetter() {
-    if (index < text.length) {
-      typedElement.textContent += text.charAt(index);
-      index++;
-      setTimeout(typeLetter, 100);
-    }
+  const storedTheme = localStorage.getItem("theme") || "light-theme";
+  document.body.className = storedTheme;
+});
+
+document.getElementById("toggleTheme").addEventListener("click", function () {
+  if (document.body.classList.contains("light-theme")) {
+    document.body.classList.remove("light-theme");
+    document.body.classList.add("dark-theme");
+    localStorage.setItem("theme", "dark-theme");
+  } else {
+    document.body.classList.remove("dark-theme");
+    document.body.classList.add("light-theme");
+    localStorage.setItem("theme", "light-theme");
   }
-  typeLetter();
+});
 
-  // Add this code to your existing script.js file
+const typedElement = document.querySelector(".typed-animation h1");
+typedElement.style.color = "#ffffff";
+const text = "NITw Marketplace Welcomes you!";
+let index = 0;
 
-// When the DOM is fully loaded
+function typeLetter() {
+  if (index < text.length) {
+    typedElement.textContent += text.charAt(index);
+    index++;
+    setTimeout(typeLetter, 100);
+  }
+}
+typeLetter();
+
 document.addEventListener('DOMContentLoaded', function() {
-  // Get all FAQ items
   const faqItems = document.querySelectorAll('.faq-item');
-  
-  // Add click event listener to each FAQ item
   faqItems.forEach(item => {
     const question = item.querySelector('.faq-question');
-    
     question.addEventListener('click', () => {
-      // Toggle the active class on the clicked item
       item.classList.toggle('active');
-      
-      // Close other items
       faqItems.forEach(otherItem => {
         if (otherItem !== item && otherItem.classList.contains('active')) {
           otherItem.classList.remove('active');
